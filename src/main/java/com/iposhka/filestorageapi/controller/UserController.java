5672425bbc4394252c94a2f8c5d0d9a90f39a4dd
Yaroslav Kelesh <yaroslavkusb@gmail.com>
2025-02-28 17:48:26 +0300

    Feat: Add "/user/me" endpoint

--- src/main/java/com/iposhka/filestorageapi/controller/UserController.java
+++ src/main/java/com/iposhka/filestorageapi/controller/UserController.java
@@ -1,10 +1,22 @@
 package com.iposhka.filestorageapi.controller;
 
+import com.iposhka.filestorageapi.dto.responce.UserResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/user")
 public class UserController {
+    @GetMapping("/me")
+    public ResponseEntity<UserResponseDto> getUser(@AuthenticationPrincipal UserDetails userDetails) {
+        UserResponseDto userResponseDto = UserResponseDto.builder()
+                .username(userDetails.getUsername())
+                .build();
 
+        return ResponseEntity.ok(userResponseDto);
+    }
 }
