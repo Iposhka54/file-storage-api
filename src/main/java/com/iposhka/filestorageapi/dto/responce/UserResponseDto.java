d40baf304af7f583f7c3e7be86d498d26caef108
Yaroslav Kelesh <yaroslavkusb@gmail.com>
2025-02-28 14:27:43 +0300

    Fix: add id in UserResponseDto, but don't give him in response

--- src/main/java/com/iposhka/filestorageapi/dto/responce/UserResponseDto.java
+++ src/main/java/com/iposhka/filestorageapi/dto/responce/UserResponseDto.java
@@ -1,3 +1,14 @@
 package com.iposhka.filestorageapi.dto.responce;
 
-public record UserResponseDto(String username) {}
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserResponseDto {
+    @JsonIgnore
+    private int id;
+
+    private String username;
+}
