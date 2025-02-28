f8e4c1de1d5e13095f1a9bd6c405cbf4e629477b
Yaroslav Kelesh <yaroslavkusb@gmail.com>
2025-02-28 14:30:21 +0300

    Feat: add handlers exceptions

--- src/main/java/com/iposhka/filestorageapi/ApplicationExceptionHandler.java
+++ src/main/java/com/iposhka/filestorageapi/ApplicationExceptionHandler.java
@@ -1,15 +1,50 @@
 package com.iposhka.filestorageapi;
 
 import com.iposhka.filestorageapi.dto.responce.ErrorResponseDto;
-import com.iposhka.filestorageapi.exception.UserAlreadyExistException;
+import com.iposhka.filestorageapi.exception.DatabaseException;
+import com.iposhka.filestorageapi.exception.UserAlreadyExistsException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
-    @ExceptionHandler({UserAlreadyExistException.class})
-    public ResponseEntity<ErrorResponseDto> handleUserAlreadyExistException(UserAlreadyExistException e){
-        return ResponseEntity.
+    private static final String BAD_REQUEST_MESSAGE = "Request body is empty or invalid. Please provide required fields.";
+    private static final String UNAUTHORIZED_MESSAGE = "Invalid username or email";
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleValidationException(MethodArgumentNotValidException e) {
+        ErrorResponseDto badRequestError = new ErrorResponseDto(BAD_REQUEST_MESSAGE);
+
+        e.getBindingResult().getFieldErrors()
+                .forEach(error -> badRequestError.putError(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(badRequestError);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDto> handleUserAlreadyExistsException(UserAlreadyExistsException e) {
+        log.error(e.getMessage());
+
+        ErrorResponseDto conflictError = new ErrorResponseDto(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(conflictError);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponseDto> handleBadCredentialsException(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponseDto(UNAUTHORIZED_MESSAGE));
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<ErrorResponseDto> handleDatabaseException(Exception e) {
+        log.error("Database error while saving user: {}", e.getMessage());
+
+        return ResponseEntity.internalServerError().body(new ErrorResponseDto(e.getMessage()));
     }
 }
