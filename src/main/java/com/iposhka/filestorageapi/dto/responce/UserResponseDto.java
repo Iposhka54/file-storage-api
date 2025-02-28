bc9e5eda9d087d27d7df46edeb6ec14f39c6475e
Yaroslav Kelesh <yaroslavkusb@gmail.com>
2025-02-28 17:47:51 +0300

    Fix: Delete unused field(id)

--- src/main/java/com/iposhka/filestorageapi/dto/responce/UserResponseDto.java
+++ src/main/java/com/iposhka/filestorageapi/dto/responce/UserResponseDto.java
@@ -1,14 +1,10 @@
 package com.iposhka.filestorageapi.dto.responce;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class UserResponseDto {
-    @JsonIgnore
-    private int id;
-
     private String username;
 }
