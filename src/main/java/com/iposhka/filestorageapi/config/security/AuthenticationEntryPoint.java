822bb636a07384a7e679272a592d86e204722423
Yaroslav Kelesh <yaroslavkusb@gmail.com>
2025-03-14 23:59:48 +0300

    Refactor: because i delete webConfig with json format add json format to response

--- src/main/java/com/iposhka/filestorageapi/config/security/AuthenticationEntryPoint.java
+++ src/main/java/com/iposhka/filestorageapi/config/security/AuthenticationEntryPoint.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
@@ -23,6 +24,7 @@ public class AuthenticationEntryPoint implements org.springframework.security.we
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         objectMapper.writeValue(response.getWriter(), AUTHENTICATION_ERROR);
     }
 }
